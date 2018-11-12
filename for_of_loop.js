09e3e23557a0c27f04305ef14a4f91f32b40c1ce
Yuri Becker <becker.yr@gmail.com>
2018-11-12 13:07:50 -0200

    For of Loop

---
+++ for_of_loop.js
@@ -0,0 +1,15 @@
+let numbers = [11111, 22222, 33333]
+let total = 0
+
+for (const number of numbers) {
+  console.log(number)
+  total += number
+}
+
+console.log(total)
+
+let name = 'Yuri Becker'
+
+for (const char of name) {
+  console.log(char)
+}
