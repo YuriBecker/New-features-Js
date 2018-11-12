1acf4c3373111e09dbb020f605d8bda7d9b4176b
Yuri Becker <becker.yr@gmail.com>
2018-11-12 13:08:33 -0200

    Template String

---
+++ template_string.js
@@ -0,0 +1,15 @@
+// Template Literals
+let word1 = 'Yuri'
+let word2 = 'Becker'
+
+const fullname = `${word1} ${word2}`
+console.log(fullname)
+
+let num1 = 10
+let num2 = 15
+
+console.log(`Sum: ${num1 + num2}`)
+
+console.log(`${word1}
+
+${word2}`)
